dd532ae6ca22e0ef6505d22e11c63dd58ae20b67
Mojahid Islam <mdmojahid2676@gmail.com>
2026-09-09 16:49:18 +0600

    Addeded to add Money feature to ui

--- js/login.js
+++ js/login.js
@@ -7,13 +7,13 @@ const pinNumber = document.getElementById('pin-number').value;
 console.log(phoneNumber,pinNumber);
 
 //Step - 4: temporary pohne and pin check 
-if(phoneNumber=== '01300294544' && pinNumber === '1234'){
+if(phoneNumber === '01300294544' && pinNumber === '1234'){
   console.log("You are Logged in");
-  window.location.href = '/home.html'
+  window.location.href = '/home.html';
   // step -5 : allow user to use the website 
 }
 else{
-  console.log("Wrong phone number or Pin")
-  alert('Enter Valid Phone or Pin Number')
+  console.log("Wrong phone number or Pin");
+  alert('Enter Valid Phone or Pin Number');
 }
 })
